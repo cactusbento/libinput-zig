02998642b04632ead30d0148a1039890108bcb8f
cactusbento <cactusbento@outlook.com>
2024-02-02 13:32:20 -0500

    Fix build

--- build.zig
+++ build.zig
@@ -4,64 +4,37 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     // Export Module for pkg mgmt.
-    const mod = b.addModule("libinput", std.Build.CreateModuleOptions{ .source_file = .{ .path = "src/libinput_core.zig" } });
+    const mod = b.addModule("libinput", .{
+        .root_source_file = .{ .path = "src/libinput_core.zig" },
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    mod.linkSystemLibrary("libinput", .{
+        .needed = true,
+        .preferred_link_mode = .Dynamic,
+    });
+    mod.linkSystemLibrary("libudev", .{
+        .needed = true,
+        .preferred_link_mode = .Dynamic,
+    });
 
     const exe = b.addExecutable(.{
         .name = "zigLibInput",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+    exe.root_module.addImport("libinput", mod);
 
-    exe.linkLibC();
-    exe.linkSystemLibrary("libinput");
-    exe.linkSystemLibrary("libudev");
-
-    exe.addModule("libinput", mod);
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    const doc_step = b.step("docs", "Build autodoc for the module");
-    const doc_test = b.addTest(.{
-        .root_source_file = .{ .path = "src/libinput_core.zig" },
-    });
-    const doc_dir = mod.builder.addInstallDirectory(.{
-        .source_dir = doc_test.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "doc/mod",
-    });
-    doc_step.dependOn(&doc_dir.step);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
